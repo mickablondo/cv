05dd9cc05e1bdd6cd11831682e05f181f4533f31
Micka Blondeau <36310658+mickablondo@users.noreply.github.com>
2023-08-20 16:33:25 +0200

    Update data.js

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const CVData = {
     personalData: {
       name: 'Mickaël Blondeau',
       title: 'Tech Lead & Blockchain Developer',
-      image: 'https://bulma.io/images/placeholders/128x128.png',
+      image: 'https://github.com/mickablondo/cv/assets/36310658/2cd167c2-2f82-43e5-8338-fd229ac9906a',
       contacts: [
         { type: 'email', value: 'mickael.blondeau@babel.coop' },
         { type: 'location', value: 'Niort, France' },
@@ -15,8 +15,8 @@ export const CVData = {
     sections: [
       {
         type: 'text',
-        title: 'Career Profile',
-        content: 'When I was child, I always want to be a developer. I think I am now. Culpa proident ad officia minim Lorem sint excepteur irure culpa nisi aliquip nostrud.Laborum voluptate nostrud fugiat occaecat proident veniam excepteur pariatur amet ex sit.Anim aliquip do in commodo adipisicing.Nulla adipisicing nisi enim ullamco sunt veniam.Ullamco labore sunt Lorem veniam id et Lorem magna eiusmod aute. Aliquip minim est consectetur anim mollit aliqua ex elit do et nulla do. Cupidatat ad sunt cillum sint consectetur sunt cillum eiusmod ad esse aliqua. Aute anim elit amet in duis aute ipsum deserunt qui deserunt officia excepteur veniam. Et laboris nostrud est ipsum nulla fugiat deserunt magna velit irure. Ullamco duis ut magna ea tempor enim in ipsum.',
+        title: 'Profil',
+        content: 'Je suis actuellement Entrepreneur-Salarié au sein de Babel.Coop. Diplômé d\'Alyra, l\'école Blockchain, je recherche principalement des projets web3.',
         icon: 'usertie'
       },
       {
@@ -25,94 +25,123 @@ export const CVData = {
         icon: 'graduation',
         items: [
           {
-            title: 'Computer Engineering (BS)',
-            authority: 'University',
-            authorityWebSite: 'https://sample.edu',
-            rightSide: '2013 - 2017'
+            title: 'Blockchain Developer (certification RS5000)',
+            authority: 'Alyra',
+            authorityWebSite: 'https://www.alyra.fr/',
+            rightSide: 'Mai à Juillet 2023'
           },
           {
-            title: 'Some Department (PHD)',
-            authority: 'Another University',
-            authorityWebSite: 'https://sample.edu',
-            rightSide: '2017 - Present'
+            title: 'Master 2 Intelligence Artificielle et Décision',
+            authority: 'Université Pierre et Marie Curie - Paris',
+            authorityWebSite: 'https://sciences.sorbonne-universite.fr/sorbonne-universite-campus-pierre-et-marie-curie',
+            rightSide: '2004 - 2005'
           }
         ]
       },
       {
         type: 'experiences-list',
         title: 'Experiences',
-        description: 'Optional',
         icon: 'archive',
         items: [
           {
-            title: 'Lead Software Developer',
-            company: 'Some Company Example INC',
-            description: 'I\'m working as a lead developer yeeeey!',
-            companyWebSite: 'http://somecompanyexample.com',
-            companyMeta: '',
-            datesBetween: '2017.10 - Present',
-            descriptionTags: ['Javascript', 'React']
+            title: 'Tech Lead',
+            company: 'SMACL Assurances',
+            description: 'Projets : CI/CD, Espace Intégration, Infrastructure As Code, Centralisation des logs. Encadrement d\'une équipe de 6 personnes.',
+            companyWebSite: 'https://www.smacl.fr/',
+            companyMeta: 'Niort',
+            datesBetween: '2016 - 2023',
+            descriptionTags: ['Java', 'Angular', 'Spring Boot', 'SQL', 'Git', 'Terraform', 'Graylog']
           },
           {
-            title: 'Software Developer',
-            company: 'Some Company Example INC',
-            description: 'I\'m using ReactJS and working as a front-end developer',
-            companyWebSite: 'http://somecompanyexample.com',
-            companyMeta: 'Little info about company',
-            datesBetween: '2016.8 - 2017.10'
+            title: 'Expert Java',
+            company: 'SMACL Assurances',
+            description: 'Développeur du socle technique.',
+            companyWebSite: 'https://www.smacl.fr/',
+            companyMeta: 'Niort',
+            datesBetween: '2014 - 2016',
+            descriptionTags: ['Java', 'Spring Boot', 'SVN']
           },
           {
-            title: 'Intern',
-            company: 'Some Software Example INC',
-            description: 'I was warming up.',
-            companyWebSite: 'http://someexamplecompany.com',
-            companyMeta: 'SF USA',
-            datesBetween: '2012.06 - 2012.10'
+            title: 'Concepteur Développeur',
+            company: 'Pôle Emploi',
+            description: 'Administration et maintenance de l’application intranet GECSI',
+            companyWebSite: 'https://www.pole-emploi.fr/',
+            companyMeta: 'Bordeaux',
+            datesBetween: '2013 - 2014',
+            descriptionTags: ['Java', 'JavaScript', 'SVN', 'SQL', 'Oracle']
+          },
+          {
+            title: 'Concepteur Développeur',
+            company: 'Cultura',
+            description: 'Développement d’un logiciel de pilotage des performances fournisseurs, d’aide à la négociation et de suivi des achats.',
+            companyWebSite: 'https://www.cultura.com/',
+            companyMeta: 'Bordeaux',
+            datesBetween: '2012 - 2013',
+            descriptionTags: ['Java', 'Play', 'jQuery', 'SVN', 'SQL', 'SQL Server']
+          },
+          {
+            title: 'Architecte logiciel junior',
+            company: 'GALEC - Groupement d\'Achats E.LECLERC',
+            description: 'Etude de la séparation du projet en différents modules.',
+            companyWebSite: 'https://www.recrutement.leclerc/societes/galec/',
+            companyMeta: 'Bordeaux',
+            datesBetween: '2012 - 2012',
+            descriptionTags: ['Java', 'Maven', 'SVN']
+          },
+          {
+            title: 'Concepteur Développeur',
+            company: 'Primobox',
+            description: 'Développement de la plateforme de dématérialisation',
+            companyWebSite: 'https://www.primobox.com/',
+            companyMeta: 'Martillac',
+            datesBetween: '2011 - 2012',
+            descriptionTags: ['Java', 'Maven', 'Git', 'SQL Server']
+          },
+          {
+            title: 'Concepteur Développeur',
+            company: 'Odigo (anciennement Prosodie)',
+            description: 'Développement sur de multiples projets : CCDS Cerbère, Pôle Emploi, Eastpak, RATP-IXXI, Bouygues Telecom EPG-TV, Fonds de solidarité, SACHEM',
+            companyWebSite: 'https://www.odigo.com/',
+            companyMeta: 'Rouen',
+            datesBetween: '2007 - 2011',
+            descriptionTags: ['Java', 'Struts', 'SVN', 'SQL Server', 'PostGresql']
+          },
+          {
+            title: 'Concepteur Développeur',
+            company: 'Orange Business (anciennement Orange Equant)',
+            description: 'Mise en œuvre de flux EAI en s’appuyant sur une solution webMethods et un framework Equant.',
+            companyWebSite: 'https://www.orange-business.com/',
+            companyMeta: 'Rennes',
+            datesBetween: '2006 - 2007',
+            descriptionTags: ['Java', 'Web Services Flow Language', 'CVS', 'Oracle']
+          },
+          {
+            title: 'Stagiaire Ingénieur études et développement',
+            company: 'VIEL & Cie',
+            description: 'Réalisation d’un produit de gestion automatisée d’un portefeuille boursier avec calculs de résultats et performances.',
+            companyWebSite: 'https://www.viel.com/',
+            companyMeta: 'Paris',
+            datesBetween: '2005',
+            descriptionTags: ['Javascript', 'PHP', 'Java J2ME', 'SQL Server']
           }
         ]
       },
       {
         type: 'projects-list',
-        title: 'Projects',
-        description: 'Optional',
+        title: 'Projets',
         icon: 'tasks',
         groups: [
           {
-            sectionHeader: 'Company Name',
-            description: 'Optional',
+            sectionHeader: 'Alyra',
+            description: 'Formation blockchain',
             items: [
-              { title: 'Project', projectUrl: 'optional', description: 'Optional' },
-              { title: 'Project', projectUrl: 'optional', description: 'Optional' },
-              { title: 'Project', projectUrl: 'optional', description: 'Optional' }
+              { title: 'PadelConnect', projectUrl: 'https://github.com/mickablondo/PadelConnect', description: 'PadelConnect est une application décentralisée de gestion de tournois de Padel permettant de créer une communauté autour de ce sport en proposant notamment un réseau social décentralisé.' },
+              { title: 'Voting-dApp', projectUrl: 'https://github.com/mickablondo/Voting-dApp', description: 'Application décentralisée de vote.' }
             ]
           }
         ]
       },
-      {
-        type: 'common-list',
-        title: 'Conferences & Certificates',
-        description: '',
-        icon: 'comments',
-        items: [
-          {
-            title: 'Some Conferences / 2019',
-            authority: 'SomeConf',
-            authorityWebSite: 'https://www.someconf.somesome'
-          },
-          {
-            title: 'Some Conferences / 2019',
-            authority: 'SomeConf',
-            authorityMeta: 'Speaker',
-            authorityWebSite: 'https://www.someconf.somesome',
-            rightSide: 'test'
-          },
-          {
-            title: 'Some Conferences / 2012',
-            authorityMeta: 'Speaker'
-          }
-        ]
-      },
-      {
+      /*{
         type: 'common-list',
         title: 'Languages',
         icon: 'language',
@@ -126,18 +155,18 @@ export const CVData = {
             authorityMeta: 'Beginner'
           }
         ]
-      },
+      },*/
       {
         type: 'tag-list',
         title: 'Skills Proficiency',
         icon: 'rocket',
-        items: ['React', 'Javascript', 'CSS', 'SQL', 'SomeTech', 'CoolTech']
+        items: ['Java', 'Solidity', 'React', 'Angular', 'SQL', 'Hardhat', 'Truffle']
       },
       {
         type: 'tag-list',
-        title: 'Hobbies & Interests',
+        title: 'Centres d\'intérêt',
         icon: 'cubes',
-        items: ['Photography', 'Poetry']
+        items: ['Padel', 'Basket', 'Tennis', 'Cinéma', 'Lecture']
       }
     ]
   }
